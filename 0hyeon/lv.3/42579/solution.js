e2b1511ece612509c2ef2f5d30cdb08ce9e6905a
0hyeon <djdjdjk2006@naver.com>
2023-02-16 14:56:28 +0900

    feat:lv0 진료순서 정하기

--- 0hyeon/lv.3/42579/solution.js
+++ 0hyeon/lv.3/42579/solution.js
@@ -1,11 +1,12 @@
+const genres = ["classics","pop","classics","classics","pop"];
+const plays = [500,600,150,800,2500]
 function solution(genres, plays) {
   const genreMap = new Map();
-  
-  //묶어주기구현
-  genres
-      .map((genre,index)=> [genre,plays[index]])
-      .forEach(([genre,play],index)=>{
-          const data = genreMap.get(genre) || {total:0,songs:[]};
+      //Map setting
+      genres
+      .map((genre,index)=> [genre,plays[index]])//index + plays 
+      .forEach(([genre,play],index) => {//장르,플레이수 꺼내고
+          const data = genreMap.get(genre)||{total:0,songs:[]}// 키값을 꺼내고 없으면 || 뒤에 초기세팅
           genreMap.set(genre,{
               total: data.total + play,
               songs: [...data.songs, {play,index}]
@@ -13,8 +14,13 @@ function solution(genres, plays) {
                   .slice(0,2)
           })
       })
+      //Map사용
       return [...genreMap.entries()]
-          .sort((a,b)=>b[1].total - a[1].total)
-          .flatMap((item)=>item[1].songs)
-          .map((song)=>song.index)
+        .sort((a,b)=>b[1].total - a[1].total)
+        .flatMap((item)=>item[1].songs)
+        .map((song)=>song.index)
+  
 }
+console.log(
+  solution(genres,plays)
+)
